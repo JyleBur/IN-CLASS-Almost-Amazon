512a7e5f61d26d2ecda33768b5c908739d166c62
Kyle B <kberry1298@gmail.com>
2023-06-03 12:36:45 -0500

    almost amazon

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,19 +1,24 @@
+// utils/startApp.js
+
+import { getBooks } from '../api/bookData';
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
 import navBar from '../components/shared/navBar';
 import domEvents from '../events/domEvents';
 import formEvents from '../events/formEvents';
 import navigationEvents from '../events/navigationEvents';
+import { showBooks } from '../pages/books';
 
-const startApp = () => {
-  domBuilder(); // BUILD THE DOM
-  domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
-  formEvents(); // ADD FORM EVENT LISTENTERS TO THE DOM
-  navBar(); // DYNAMICALLY ADD THE NAV
-  logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
-  navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+const startApp = (user) => {
+  domBuilder(user); // ADD USER SO THAT YOU CAN UPDATE CALLS
+  domEvents(user); // ADD USER SO THAT YOU CAN UPDATE CALLS
+  formEvents(user); // ADD USER SO THAT YOU CAN UPDATE CALLS
+  navBar();
+  logoutButton();
+  navigationEvents();
 
   // TODO: Put all books on the DOM on App load
+  getBooks(user.uid).then((books) => showBooks(books));
 };
 
 export default startApp;
